c3756d3bb1e90a8689ef809208756487e903d97d
Phelyks <flexxmosh@gmail.com>
2023-08-14 12:34:31 +0300

    update nav css & javascript

---
+++ Assets/index.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded",function(){
+    var navLinks = document.querySelectorAll("#navList li");
+
+    navLinks.forEach(function(link){
+        link.addEventListener("click",function(event){
+            event.preventDefault(); //Prevent the default link behavior
+
+            removeActiveClass(); //Remove active class from all links
+
+            link.classList.add("active"); //Add active class to the clicked link
+        });
+    });
+
+    function removeActiveClass(){
+        navLinks.forEach(function(link){
+            link.classList.remove("active");
+        });
+    }
+
+});
