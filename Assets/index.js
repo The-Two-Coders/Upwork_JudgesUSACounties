d22368797acdd796d3ffd15e74e26ebeb1ea7260
Phelyks <flexxmosh@gmail.com>
2023-08-24 22:11:59 +0300

    update

--- Assets/index.js
+++ Assets/index.js
@@ -1,20 +1,55 @@
 document.addEventListener("DOMContentLoaded",function(){
-    var navLinks = document.querySelectorAll("#navList li");
+    var navLinks = document.querySelectorAll("#navList .list-items");
 
-    navLinks.forEach(function(link){
-        link.addEventListener("click",function(event){
-            event.preventDefault(); //Prevent the default link behavior
+    navLinks = forEach(function(link){
+        link.addEventListener('click',function(event){
+            event.preventDefault(); // Prevent the default link behavior
 
             removeActiveClass(); //Remove active class from all links
 
             link.classList.add("active"); //Add active class to the clicked link
         });
     });
-
     function removeActiveClass(){
         navLinks.forEach(function(link){
             link.classList.remove("active");
         });
     }
-
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// document.addEventListener("DOMContentLoaded",function(){
+//     var navLinks = document.querySelectorAll("#navList li");
+
+//     navLinks.forEach(function(link){
+//         link.addEventListener("click",function(event){
+//             event.preventDefault(); //Prevent the default link behavior
+
+//             removeActiveClass(); //Remove active class from all links
+
+//             link.classList.add("active"); //Add active class to the clicked link
+//         });
+//     });
+
+//     function removeActiveClass(){
+//         navLinks.forEach(function(link){
+//             link.classList.remove("active");
+//         });
+//     }
+
+// });
